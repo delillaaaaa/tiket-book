03ce4b5ea876c03419d579ac5a24ccf2976e3392
Delilla Anandita <delillaanandita@gmail.com>
2025-12-26 13:55:30 +0700

    Delete tiketkonserr directory

--- tiketkonserr/EO/hapus_konser.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../db.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM konser WHERE ID_Konser='$id'");
-header('Location: dashboard.php');
-?>
--- tiketkonserr/EO/logout_eo.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['eo']);
-session_destroy();
-header('Location: login_eo.php');
-exit;
-?>
